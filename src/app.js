ad30599b81586c4ce96d0da0f33d07f47353d3e4
azul-de-prusia <tegthyn_600@hotmail.com>
2019-10-22 15:09:01 -0500

    first drawing function issues fixed

--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
 
-let color = "#000000";
-let size = 10;
-let paint = false;
-
+var color = "#000000";
+var size = 10;
+var paint = false;
 
 
 function painting (event){
@@ -10,6 +9,7 @@ function painting (event){
     let ctx = canvas.getContext("2d"); //context here, is the place where we draw
     let x = event.clientX; // axis x of the plane
     let y = event.clientY; // axis y of the plane
+    console.log(paint);
     if (paint) {
         ctx.fillStyle = color;
         ctx.fillRect(x, y, size, size);
@@ -23,14 +23,6 @@ function activate(){
 
 function deactivate(){
 paint = false;
-}
-
-function eraser (){
-
-}
-
-function pencil (){
-
 }
 
 // var add = ( x, y ) => x + y;
